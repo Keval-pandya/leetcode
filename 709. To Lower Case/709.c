1c89c641c648329a1cf4e08bd842a77425b03831
Keval Pandya <62025534+Keval-pandya@users.noreply.github.com>
2022-09-23 11:55:23 +0530

    Create 709.c

---
+++ 709. To Lower Case/709.c
@@ -0,0 +1,13 @@
+
+
+char * toLowerCase(char * s){
+    int i;
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]>='A' && s[i]<='Z')
+        {
+            s[i]=s[i]+32;
+        }
+    }
+return s;
+}
