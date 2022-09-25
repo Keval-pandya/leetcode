54b4b18a8fe93f5da39492257a6eb3c728aafd25
Keval Pandya <62025534+Keval-pandya@users.noreply.github.com>
2022-09-26 01:18:45 +0530

    Create 136.py

---
+++ 136. Single Number/136.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums:
+            if nums.count(i)>1:
+                continue
+            else:
+                return i
+                break 
+                
+        
