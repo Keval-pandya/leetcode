2ca6c9cbb3af7813bf1198b1149d2599e4dbc343
Keval Pandya <62025534+Keval-pandya@users.noreply.github.com>
2022-09-26 11:47:44 +0530

    Create 509.py

---
+++ 509. Fibonacci Number/509.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        a=0
+        b=1
+        for i in range(n):
+            c=a+b
+            a=b
+            b=c
+        return a
+        
+        
